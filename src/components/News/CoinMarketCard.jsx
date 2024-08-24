4c7488a5bc33e72902910507e588e85457daef54
Barronakn <barronakn@gmail.com>
2024-08-24 05:36:33 +0100

    Updated CoinMarketCard component with dark mode styles and adjusted grid layout in News page

--- src/components/News/CoinMarketCard.jsx
+++ src/components/News/CoinMarketCard.jsx
@@ -5,30 +5,32 @@ import heart from '../../assets/icons/heart.png';
 
 const CoinMarketCard = () => {
   return (
-    <section className="flex flex-col gap-2.5 p-2.5">
+    <section className="flex flex-col gap-2.5 p-2.5 dark:bg-dark-blue-2 rounded-xl">
       <div className="flex flex-row gap-2 items-center">
         <div>
           <img src={coin_market} alt="coinmarket-icon" />
         </div>
         <div>
-          <p className="text-xs dark:text-white font-semibold">CoinMarketCap</p>
-          <span className="text-xs text-dark-gray">News - 7 hours ago</span>
+          <p className="text-xs font-semibold dark:text-white">CoinMarketCap</p>
+          <span className="text-xs dark:text-light-gray text-dark-gray">News - 7 hours ago</span>
         </div>
       </div>
 
-      <div className="bg-light-gray h-48.5 w-64.5 rounded-10"></div>
+      <div>
+        <div className="bg-light-gray h-48.5 w-54 rounded-10"></div>
+      </div>
 
       <div className="flex flex-col gap-1.5">
-        <h4 className="text-xs italic font-semibold">Ethereum’s Merge Coming and the Stakes Couldn’t Be Higher</h4>
-        <p className="text-xs font-medium">The most important upgrade in blockchain history is slated for August. And the outcome of Ethe....</p>
+        <h4 className="text-xs italic font-semibold dark:text-white">Ethereum’s Merge Coming and the Stakes Couldn’t Be Higher</h4>
+        <p className="text-xs font-medium dark:text-white">The most important upgrade in blockchain history is slated for August. And the outcome of Ethe....</p>
         <div className="flex flex-row gap-2.5">
           <div className="flex flex-row gap-1">
             <img src={heart} alt="heart-icon" />
-            <span className="text-sm font-medium">5</span>
+            <span className="text-sm font-medium dark:text-white">5</span>
           </div>
           <div className="flex flex-row gap-1">
             <img src={comment_icon} alt="comment-icon" />
-            <span className="text-sm font-medium">5</span>
+            <span className="text-sm font-medium dark:text-white">5</span>
           </div>
         </div>
       </div>
--- src/pages/News.jsx
+++ src/pages/News.jsx
@@ -7,7 +7,7 @@ const News = () => {
   return (
     <div className="border border-gray p-6 pt-22 dark:bg-black">
       <h3 className="text-base font-semibold mb-8 dark:text-white">Latest crypto news</h3>
-      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-3 mb-5 mt-22">
+      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-3 mb-5 mt-22">
         <CoinMarketCard />
         <CoinMarketCard />
         <CoinMarketCard />
@@ -18,8 +18,8 @@ const News = () => {
         <CoinMarketCard />
       </div>
       <Link className="text-center" to="#">
-        <div class="flex flex-row mx-auto my-0 w-fit items-center gap-1 py-2.5 px-3.5  rounded-100 text-sm bg-gray bg-opacity-50">
-          <span>Learn more</span>
+        <div class="flex flex-row mx-auto my-0 w-fit items-center gap-1 py-2.5 px-3.5  rounded-100 text-sm bg-gray bg-opacity-50 dark:bg-dark-blue-2">
+          <span className="dark:text-white">Learn more</span>
           <img src={arrow_down} alt="arrow_down-icon" />
         </div>
       </Link>
